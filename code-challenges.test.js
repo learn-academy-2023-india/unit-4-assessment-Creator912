6a1f325f5c5995aac67d15755e11946fdc9d349f
Creator912 <walker.joshua912@yahoo.com>
2024-02-27 17:15:41 -0800

    Completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -20,15 +20,17 @@ const fibonacci = [0, 1];
 const length = 8;
 const fibonacciSequenceFrom3 = fibonacciArr(length);
 
-// Comment- Didn't realize until aftewards I probably needed to create a test to do this question the way you guys wanted it done. I used the resources I had to come up with this method. Not really sure how fibonacci is truly suppose to work.
+// // Comment- Didn't realize until aftewards I probably needed to create a test to do this question the way you guys wanted it done. I used the resources I had to come up with this method. Not really sure how fibonacci is truly suppose to work.
 
-// a) Create a test with expect statements for each of the variables provided. 
+// // a) Create a test with expect statements for each of the variables provided. 
 
 describe("fibonaccilength", () => {
-  it("returns the number it multiplied by the number after", () => {
-    expect(fibonacci()).toEqual("")
+  it("returns an array of the next numbers in fibonacci order ", () => {
+    expect(fibonacci(6)).toEqual([1, 1, 2, 3, 5, 8])
+    expect(fibonacci(10)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+
   })
-})
+  })
 const fibonacciLength1 = 6
 // Expected output: [1, 1, 2, 3, 5, 8]
 
@@ -36,12 +38,55 @@ const fibonacciLength2 = 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
 // b) Create the function that makes the test pass.
+      
+const createFibonnaci = (number) => {
+    const MyArray = [0,1]
+    let a,b;
+    
+for (let i = 2; i < number; i++) {
+    a = MyArray[i-1]
+    b = MyArray[i-2]
+    MyArray.push(a + b)
+}
+    return MyArray
+}
+// console.log(createFibonnaci(6));
+// console.log(createFibonnaci(10));
 
-// Pseudo code:
+
+
+// Pseudo code: 
+// I created a function named fibonnaci that can take a number
+// I make an array containing the first 2 digits in fibonnaci 
+// I make a variable let that will allow my variable to be reasssigned
+// I then make a for loop that iterates through my function
+// I then use a .push method to call the argument through my for loop that will return my array updated array
+
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
 
+function Election(votes) {
+  const tally = votes.up - votes.down;
+  return tally;
+}
+
+const newvotes = { up: n1, down: n2 };
+const newtally = Election(votes);
+
+
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("electiontally", () => {
+  it("returns the difference of up votes to down votes", () => {
+    const votes1 = { upVotes: 13, downVotes: 2 }
+    const votes2 = { upVotes: 2, downVotes: 33 }
+    expect(election1).toEqual([11])
+    expect(election2).toEqual([-31])
+
+
+
+  })
+  })
 
 const votes1 = { upVotes: 13, downVotes: 2 }
 // Expected output: 11
@@ -50,4 +95,18 @@ const votes2 = { upVotes: 2, downVotes: 33 }
 
 // b) Create the function that makes the test pass.
 
-// Pseudo code:
+function electionCount(votes) {
+  const tally = votes.up - votes.down;
+  return tally;
+}
+
+  const votes = { up: 13, down: 2 };
+  const tally = calculateelection(votes);
+
+  
+
+
+// Pseudo code: 
+// I created an object that can take any vote
+// I then make a variable that can tally up votes and down votes difference
+// I return the end tally
